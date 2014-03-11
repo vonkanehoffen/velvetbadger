30a132adedbc8d3b834dce34befae470bf8c4945
Kane Clover <kane@assure24.com>
2014-03-11 14:43:41 +0000

    folding tickets and info content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
 // Velvet Badger: Main JS
 
+function to_ajax_id(str) {
+  return (str.replace(/[^a-z0-9]/gi,''))+'-ajax';
+}
+
+function scroll_to(selector) {
+  $('html,body').animate({
+    scrollTop: $(selector).offset().top
+  }, 800);
+  return false;
+}
+
 $(function(){
-	console.log("here");
+	$('#btn-info').click(function(e){
+		$('#tickets').slideUp(300);
+		$('#info').slideDown(300);
+		$('#footer').show();
+		scroll_to('#info');
+	});
+	$('#btn-tickets').click(function(e){
+		$('#info').slideUp(300);
+		$('#tickets').slideDown(300);
+		$('#footer').show();
+		scroll_to('#tickets');
+	});
+
 });
